981d53ca77b739bcaeccf33a3509ec04ad70a542
Sowmya U B <sowmya.ub@technoforte.co.in>
2026-02-11 17:52:19 +0530

    code changes

--- authentication/authentication-core/src/main/java/io/mosip/authentication/core/constant/IdAuthenticationErrorConstants.java
+++ authentication/authentication-core/src/main/java/io/mosip/authentication/core/constant/IdAuthenticationErrorConstants.java
@@ -154,7 +154,7 @@ public enum IdAuthenticationErrorConstants {
 			"Please try adding all the required path parameters."),
 	PARTNER_API_EXPIRED("IDA-MPA-039", "Partner API is expired or using before Commence Start Date."),
 	AUTH_CHARGES_NOT_AVAILABLE("IDA-MPA-040", "Auth charges not available for auth type and sub type."),
-	PARTNER_CURRENT_BALANCE_AVAILABLE("IDA-MPA-041", "Current  Balance not availabel for partner."),
+	PARTNER_CURRENT_BALANCE_AVAILABLE("IDA-MPA-041", "Current  Balance not available for partner."),
 	PARTNER_INSUFFICIENT_BALANCE("IDA-MPA-041", "Partner balance is less than required authentication charge."),
 
 	DATA_VALIDATION_FAILED("IDA-IDV-001", "Input Data Validation Failed"),
