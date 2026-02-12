b2d67272ecdd985c84ab2f480383ba31a4ee8318
malaypandey-technoforte <malay.pandey@technoforte.co.in>
2026-02-12 17:12:17 +0530

    Add is_processed column to partner_payment_transactions table
    Added 'is_processed' column to track payment transaction processing status.

--- db_scripts/mosip_ida/ddl/ida-partner_payment_transactions.sql
+++ db_scripts/mosip_ida/ddl/ida-partner_payment_transactions.sql
@@ -3,6 +3,7 @@ CREATE TABLE ida.partner_payment_transactions (
 	amount numeric NOT NULL,
 	partner_id character varying(128) NOT NULL,
     log_dtimes timestamp NOT NULL,
+	is_processed boolean NOT NULL DEFAULT false, 
 	CONSTRAINT  pk_idTransactionId PRIMARY KEY (transaction_id)
 );
 
@@ -11,5 +12,6 @@ COMMENT ON COLUMN ida.partner_payment_transactions.partner_id IS 'Unique identif
 COMMENT ON COLUMN ida.partner_payment_transactions.amount IS 'Transaction amount for the partner payment.';
 COMMENT ON COLUMN ida.partner_payment_transactions.transaction_id IS 'Unique identifier of the payment transaction.';
 COMMENT ON COLUMN ida.partner_payment_transactions.log_dtimes IS 'Timestamp when the payment transaction was logged in the system.';
+COMMENT ON COLUMN ida.partner_payment_transactions.is_processed IS 'Flag to track if the payment transaction has been processed for idempotency.';
 
 
