fba62616007a4b0d7f9e2e6d518b9f86c3537f36
Karthik-TF <karthik.s@technoforte.co.in>
2026-04-02 18:13:12 +0530

    Revert "auth trns sync"

--- authentication/authentication-common/src/test/java/io/mosip/authentication/common/service/impl/IdServiceImplTest.java
+++ authentication/authentication-common/src/test/java/io/mosip/authentication/common/service/impl/IdServiceImplTest.java
@@ -11,7 +11,6 @@ import java.util.*;
 import java.util.Map.Entry;
 import java.util.stream.Collectors;
 
-import io.mosip.authentication.common.service.websub.impl.AuthTransactionEventPublisher;
 import org.hibernate.exception.JDBCConnectionException;
 import org.junit.Assert;
 import org.junit.Test;
@@ -50,11 +49,7 @@ public class IdServiceImplTest {
 
 	@Mock
 	private AutnTxnRepository autntxnrepository;
-
-	@Mock
-	private AuthTransactionEventPublisher authTransactionEventPublisher;
-
-
+	
 	@Test
 	public void getIdentityTest1() throws IdAuthenticationBusinessException, IOException {
 		String uin = "12312312";
