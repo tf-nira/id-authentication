ba4f57f6bc7a8bcf4419e2d9312a88527b73c873
Karthik-TF <karthik.s@technoforte.co.in>
2026-04-03 17:31:11 +0530

    Revert "Revert "vid""

--- authentication/authentication-common/src/test/java/io/mosip/authentication/common/service/impl/IdServiceImplTest.java
+++ authentication/authentication-common/src/test/java/io/mosip/authentication/common/service/impl/IdServiceImplTest.java
@@ -3,6 +3,7 @@ package io.mosip.authentication.common.service.impl;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
+import static org.mockito.ArgumentMatchers.any;
 
 import java.io.IOException;
 import java.time.LocalDateTime;
@@ -260,7 +261,8 @@ public class IdServiceImplTest {
 		IdServiceImpl idServiceSpy = Mockito.spy(idServiceImpl);
 		Mockito.doReturn(null).when(idServiceSpy).getIdByVid(idvId, isBio, filterAttributes);
 		String idvIdType = "VID";
-		Mockito.doThrow(JDBCConnectionException.class).when(identityRepo).deleteById("11");
+		//Mockito.doThrow(JDBCConnectionException.class).when(identityRepo).deleteById("11");
+		Mockito.doThrow(JDBCConnectionException.class).when(identityRepo).save(Mockito.any());
 		idServiceSpy.processIdType(idvIdType, idvId, isBio, markVidConsumed, filterAttributes);
 	}
 
@@ -317,7 +319,8 @@ public class IdServiceImplTest {
 		entityOpt.get().setTransactionLimit(0);
 		Mockito.when(identityRepo.findById(vid)).thenReturn(entityOpt);
 		ReflectionTestUtils.invokeMethod(idServiceImpl, "updateVIDstatus", vid);
-		Mockito.verify(identityRepo, Mockito.times(1)).deleteById(vid);
+		//Mockito.verify(identityRepo, Mockito.times(1)).deleteById(vid);
+		Mockito.verify(identityRepo).save(any(IdentityEntity.class));
 	}
 
 	/**
@@ -353,7 +356,8 @@ public class IdServiceImplTest {
 		entityOpt.get().setTransactionLimit(1);
 		Mockito.when(identityRepo.findById(vid)).thenReturn(entityOpt);
 		ReflectionTestUtils.invokeMethod(idServiceImpl, "updateVIDstatus", vid);
-		Mockito.verify(identityRepo, Mockito.times(1)).deleteById(vid);
+		//Mockito.verify(identityRepo, Mockito.times(1)).deleteById(vid);
+		Mockito.verify(identityRepo).save(any(IdentityEntity.class));
 	}
 
 	/**
@@ -371,7 +375,8 @@ public class IdServiceImplTest {
 		entityOpt.get().setTransactionLimit(-1);
 		Mockito.when(identityRepo.findById(vid)).thenReturn(entityOpt);
 		ReflectionTestUtils.invokeMethod(idServiceImpl, "updateVIDstatus", vid);
-		Mockito.verify(identityRepo, Mockito.times(1)).deleteById(vid);
+		//Mockito.verify(identityRepo, Mockito.times(1)).deleteById(vid);
+		Mockito.verify(identityRepo).save(any(IdentityEntity.class));
 	}
 
 	private IdentityEntity getEntity() {
