9f2491dacea12f7618772238df1a20a24bf6ef08
SowmyaGoudar <40026315+SowmyaGoudar@users.noreply.github.com>
2026-04-08 11:15:51 +0530

    Revert "Enhance NotificationServiceImpl with logging"

--- authentication/authentication-common/src/main/java/io/mosip/authentication/common/service/impl/notification/NotificationServiceImpl.java
+++ authentication/authentication-common/src/main/java/io/mosip/authentication/common/service/impl/notification/NotificationServiceImpl.java
@@ -15,8 +15,6 @@ import java.util.Set;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
@@ -56,8 +54,6 @@ import reactor.util.function.Tuples;
  */
 @Service
 public class NotificationServiceImpl implements NotificationService {
-	
-	private static final Logger logger = LoggerFactory.getLogger(NotificationServiceImpl.class);
 
 	/** The Constant AUTH_TYPE. */
 	private static final String AUTH_TYPE = "authType";
@@ -140,12 +136,6 @@ public class NotificationServiceImpl implements NotificationService {
 		}
 
 		sendNotification(values, email, phoneNumber, SenderType.AUTH, notificationType, templateLanguages);
-		logger.info("sendAuthNotification - Values map keys: {}", values.keySet());
-		logger.info("sendAuthNotification - identity value: {}", values.get("identity"));
-		logger.info("sendAuthNotification - nameMap from getIdEntityInfoMap: {}", infoHelper.getIdEntityInfoMap(DemoMatchType.NAME, idInfo, "eng"));
-		for (String lang : templateLanguages) {
-			logger.info("sendAuthNotification - name for lang {}: {}, givenName_eng: {}, surname_eng: {}", lang, values.get("name_" + lang), values.get("givenName_eng"), values.get("surname_eng"));
-		}
 	}
 
 	public void sendOTPNotification(String idvid, String idvidType, Map<String, String> valueMap,
