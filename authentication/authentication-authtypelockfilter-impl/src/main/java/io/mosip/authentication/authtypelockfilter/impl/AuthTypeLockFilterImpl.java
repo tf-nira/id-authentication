676a5f2570bf265f1195022fca42b90eabd3b601
Shivam Rajesh Gupta <56411166+shivamg9@users.noreply.github.com>
2026-04-16 15:47:27 +0530

    Enhance logging and error handling in AuthTypeLockFilterImpl
    Added logging for validation process and improved error handling.

--- authentication/authentication-authtypelockfilter-impl/src/main/java/io/mosip/authentication/authtypelockfilter/impl/AuthTypeLockFilterImpl.java
+++ authentication/authentication-authtypelockfilter-impl/src/main/java/io/mosip/authentication/authtypelockfilter/impl/AuthTypeLockFilterImpl.java
@@ -16,11 +16,13 @@ import io.mosip.authentication.core.constant.IdAuthenticationErrorConstants;
 import io.mosip.authentication.core.exception.IdAuthenticationBusinessException;
 import io.mosip.authentication.core.indauth.dto.AuthRequestDTO;
 import io.mosip.authentication.core.indauth.dto.IdentityInfoDTO;
+import io.mosip.authentication.core.logger.IdaLogger;
 import io.mosip.authentication.core.spi.authtype.status.service.AuthtypeStatusService;
 import io.mosip.authentication.core.spi.indauth.match.AuthType;
 import io.mosip.authentication.core.spi.indauth.match.IdInfoFetcher;
 import io.mosip.authentication.core.spi.indauth.match.MatchType;
 import io.mosip.idrepository.core.dto.AuthtypeStatus;
+import io.mosip.kernel.core.logger.spi.Logger;
 
 /**
  * The Class AuthTypeLockFilterImpl - implementation of auth filter for
@@ -30,6 +32,8 @@ import io.mosip.idrepository.core.dto.AuthtypeStatus;
  * @author Loganathan Sekar
  */
 public class AuthTypeLockFilterImpl implements IMosipAuthFilter {
+	
+	private static Logger logger = IdaLogger.getLogger(AuthTypeLockFilterImpl.class);
 	
 	@Autowired
 	private AuthtypeStatusService authTypeStatusService;
@@ -53,30 +57,45 @@ public class AuthTypeLockFilterImpl implements IMosipAuthFilter {
 	 */
 	public void validate(AuthRequestDTO authRequest, Map<String, List<IdentityInfoDTO>> identityData,
 			Map<String, Object> properties) throws IdAuthenticationFilterException {
-		validateAuthTypeStatus(authRequest, (String)properties.get(IdAuthCommonConstants.TOKEN));
+		String token = (String)properties.get(IdAuthCommonConstants.TOKEN);
+		logger.info("AuthTypeLockFilterImpl.validate - START for token: " + token);
+		validateAuthTypeStatus(authRequest, token);
+		logger.info("AuthTypeLockFilterImpl.validate - END for token: " + token);
 	}
 	
 	private void validateAuthTypeStatus(AuthRequestDTO authRequestDTO, String token) throws IdAuthenticationFilterException {
 		try {
+			logger.debug("AuthTypeLockFilterImpl.validateAuthTypeStatus - Fetching auth type status for token: " + token);
 			List<AuthtypeStatus> authtypeStatusList = authTypeStatusService
 					.fetchAuthtypeStatus(token);
+			logger.debug("AuthTypeLockFilterImpl.validateAuthTypeStatus - Got " + (authtypeStatusList != null ? authtypeStatusList.size() : 0) + " status records");
 			if (Objects.nonNull(authtypeStatusList) && !authtypeStatusList.isEmpty()) {
 				for (AuthtypeStatus authTypeStatus : authtypeStatusList) {
-					validateAuthTypeStatus(authRequestDTO, authTypeStatus,authtypeStatusList);
+					logger.debug("AuthTypeLockFilterImpl.validateAuthTypeStatus - Checking status: authType=" + authTypeStatus.getAuthType() + 
+						", subType=" + authTypeStatus.getAuthSubType() + ", locked=" + authTypeStatus.getLocked());
+					validateAuthTypeStatus(authRequestDTO, authTypeStatus, authtypeStatusList);
 				}
+			} else {
+				logger.debug("AuthTypeLockFilterImpl.validateAuthTypeStatus - No locked auth types found for token: " + token);
 			}
 		} catch (IdAuthenticationFilterException e) {
+			logger.info("AuthTypeLockFilterImpl.validateAuthTypeStatus - THROWING AUTH_TYPE_LOCKED exception: " + e.getErrorCode());
 			throw e;
 		} catch (IdAuthenticationBusinessException e) {
+			logger.error("AuthTypeLockFilterImpl.validateAuthTypeStatus - Error: " + e.getErrorCode() + ", " + e.getErrorText());
 			throw new IdAuthenticationFilterException(e.getErrorCode(), e.getErrorText(), e.getCause());
 		}
 	}
 
 	private void validateAuthTypeStatus(AuthRequestDTO authRequestDTO, AuthtypeStatus authTypeStatus,List<AuthtypeStatus> authtypeStatusList)
 			throws IdAuthenticationFilterException {
+		logger.debug("AuthTypeLockFilterImpl.validateAuthTypeStatus - Processing authType=" + authTypeStatus.getAuthType() + 
+			", locked=" + authTypeStatus.getLocked());
 		if (authTypeStatus.getLocked()) {
+			logger.debug("AuthTypeLockFilterImpl.validateAuthTypeStatus - Auth type is LOCKED, checking request type");
 			if (AuthTypeUtil.isDemo(authRequestDTO)
 					&& authTypeStatus.getAuthType().equalsIgnoreCase(MatchType.Category.DEMO.getType())) {
+				logger.info("AuthTypeLockFilterImpl.validateAuthTypeStatus - Throwing exception for locked DEMO");
 				throw new IdAuthenticationFilterException(
 						IdAuthenticationErrorConstants.AUTH_TYPE_LOCKED.getErrorCode(),
 						String.format(IdAuthenticationErrorConstants.AUTH_TYPE_LOCKED.getErrorMessage(),
@@ -88,6 +107,7 @@ public class AuthTypeLockFilterImpl implements IMosipAuthFilter {
 				for (AuthType authType : BioAuthType.getSingleBioAuthTypes().toArray(s -> new AuthType[s])) {
 					if (authType.getType().equalsIgnoreCase(authTypeStatus.getAuthSubType())) {
 						if (authType.isAuthTypeEnabled(authRequestDTO, idInfoFetcher)) {
+							logger.info("AuthTypeLockFilterImpl.validateAuthTypeStatus - Throwing exception for locked BIO-" + authType.getType());
 							throw new IdAuthenticationFilterException(
 									IdAuthenticationErrorConstants.AUTH_TYPE_LOCKED.getErrorCode(),
 									String.format(IdAuthenticationErrorConstants.AUTH_TYPE_LOCKED.getErrorMessage(),
@@ -102,6 +122,7 @@ public class AuthTypeLockFilterImpl implements IMosipAuthFilter {
 			else if (AuthTypeUtil.isOtp(authRequestDTO))
 				if (authTypeStatus.getAuthType().equalsIgnoreCase(MatchType.Category.OTP.getType())
 						&& (authTypeStatus.getAuthSubType() == null || authTypeStatus.getAuthSubType().isEmpty())) {
+					logger.info("AuthTypeLockFilterImpl.validateAuthTypeStatus - Throwing exception for locked OTP");
 					throw new IdAuthenticationFilterException(
 							IdAuthenticationErrorConstants.AUTH_TYPE_LOCKED.getErrorCode(),
 							String.format(IdAuthenticationErrorConstants.AUTH_TYPE_LOCKED.getErrorMessage(),
@@ -111,6 +132,7 @@ public class AuthTypeLockFilterImpl implements IMosipAuthFilter {
 							&& (authTypeStatus.getAuthSubType().equalsIgnoreCase(IdAuthCommonConstants.PHONE_NUMBER)
 									|| authTypeStatus.getAuthSubType().equalsIgnoreCase(IdAuthCommonConstants.EMAIL))
 							&& authTypeStatus.getLocked().equals(true)) {
+						logger.debug("AuthTypeLockFilterImpl.validateAuthTypeStatus - Checking Email/Phone OTP lock");
 						Optional<AuthtypeStatus> otherSubOtpTypeLocked = authtypeStatusList.stream()
 								.filter(authTypeStatus1 -> authTypeStatus1.getAuthType() != null
 										&& authTypeStatus1.getAuthType()
@@ -121,6 +143,7 @@ public class AuthTypeLockFilterImpl implements IMosipAuthFilter {
 										&& authTypeStatus1.getLocked())
 								.findAny();
 						if (otherSubOtpTypeLocked.isPresent()) {
+							logger.info("AuthTypeLockFilterImpl.validateAuthTypeStatus - Throwing exception for locked OTP (other subtype)");
 							throw new IdAuthenticationFilterException(
 									IdAuthenticationErrorConstants.AUTH_TYPE_LOCKED.getErrorCode(),
 									String.format(IdAuthenticationErrorConstants.AUTH_TYPE_LOCKED.getErrorMessage(),
@@ -132,11 +155,14 @@ public class AuthTypeLockFilterImpl implements IMosipAuthFilter {
 
 			else if (AuthTypeUtil.isPin(authRequestDTO)
 					&& authTypeStatus.getAuthType().equalsIgnoreCase(MatchType.Category.SPIN.getType())) {
+				logger.info("AuthTypeLockFilterImpl.validateAuthTypeStatus - Throwing exception for locked PIN");
 				throw new IdAuthenticationFilterException(
 						IdAuthenticationErrorConstants.AUTH_TYPE_LOCKED.getErrorCode(),
 						String.format(IdAuthenticationErrorConstants.AUTH_TYPE_LOCKED.getErrorMessage(),
 								MatchType.Category.SPIN.getType()));
 			}
+		} else {
+			logger.debug("AuthTypeLockFilterImpl.validateAuthTypeStatus - Auth type is NOT locked, allowing");
 		}
 	}
 }
