6acb95e57bccfde08499eb8be0dbdb2592513719
Satyam Rajkumar Singh <satyamsingh007007@gmail.com>
2026-05-21 16:39:14 +0530

    Add back5

--- authentication/authentication-common/src/main/java/io/mosip/authentication/common/service/integration/PartnerServiceManager.java
+++ authentication/authentication-common/src/main/java/io/mosip/authentication/common/service/integration/PartnerServiceManager.java
@@ -15,7 +15,6 @@ import javax.transaction.Transactional;
 
 import org.apache.commons.codec.digest.DigestUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.stereotype.Component;
 
 import com.fasterxml.jackson.core.JsonParseException;
