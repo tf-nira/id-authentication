30e24f7edea090f7309c6e88ac04392519a66fc7
Satyam Rajkumar Singh <satyamsingh007007@gmail.com>
2026-05-25 13:46:52 +0530

    update CacheInspectorService2.

--- authentication/authentication-common/src/main/java/io/mosip/authentication/common/service/cache/CacheInspectorService.java
+++ authentication/authentication-common/src/main/java/io/mosip/authentication/common/service/cache/CacheInspectorService.java
@@ -6,9 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.Cache;
 import org.springframework.cache.CacheManager;
 import org.springframework.cache.concurrent.ConcurrentMapCache;
-import org.springframework.stereotype.Service;
 
-@Service
 public class CacheInspectorService {
 
     @Autowired
