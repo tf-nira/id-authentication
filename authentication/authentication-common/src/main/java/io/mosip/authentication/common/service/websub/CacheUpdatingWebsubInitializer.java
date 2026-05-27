b142fbd026ce0c0cff57b36402704a39f1bef9f2
Satyam Rajkumar Singh <satyamsingh007007@gmail.com>
2026-05-27 14:35:23 +0530

    Adding ida-cache-ttl-in-days and it's working method.

--- authentication/authentication-common/src/main/java/io/mosip/authentication/common/service/websub/CacheUpdatingWebsubInitializer.java
+++ authentication/authentication-common/src/main/java/io/mosip/authentication/common/service/websub/CacheUpdatingWebsubInitializer.java
@@ -1,9 +1,7 @@
 package io.mosip.authentication.common.service.websub;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.CacheManager;
-import io.mosip.authentication.core.constant.IdAuthCommonConstants;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 import io.mosip.kernel.core.util.StringUtils;
@@ -25,9 +23,6 @@ public abstract class CacheUpdatingWebsubInitializer extends BaseIDAWebSubInitia
 	@Value("${ida-cache-ttl-in-days:1}")
 	private int cacheTtlInDays;
 
-    @Autowired
-    private CacheManager cacheManager;
-
 
     /**
 	 * Checks if is cache enabled.
@@ -43,46 +38,11 @@ public abstract class CacheUpdatingWebsubInitializer extends BaseIDAWebSubInitia
 	 *
 	 * value <= 0 → disabled
 	 */
-	protected boolean isCacheTtlEnabled() {
+    public boolean isCacheTtlEnabled() {
 		return isCacheEnabled() && cacheTtlInDays > 0;
 	}
 
-//	/**
-//	 * Returns configured cache TTL.
-//	 */
-//	protected int getCacheTtlInDays() {
-//		return cacheTtlInDays;
-//	}
-
-//	@Scheduled(
-//			//fixedDelayString = "#{${ida-cache-ttl-in-days:1} * 24 * 60 * 60 * 1000}" below in minutes.
-//			fixedDelayString = "#{${ida-cache-ttl-in-days:1} * 1000}"
-//	)
-//	public void clearCachesByTTL() {
-//
-//		if (!isCacheTtlEnabled() || cacheManager == null) {
-//			return;
-//		}
-//
-//		logger.info(
-//				IdAuthCommonConstants.IDA,
-//				getClass().getSimpleName(),
-//				"CACHE_TTL",
-//				"Starting scheduled cache eviction"
-//		);
-//
-//		for (String cacheName : cacheManager.getCacheNames()) {
-//
-//			if (cacheManager.getCache(cacheName) != null) {
-//				Objects.requireNonNull(cacheManager.getCache(cacheName)).clear();
-//
-//				logger.info(
-//						IdAuthCommonConstants.IDA,
-//						getClass().getSimpleName(),
-//						"CACHE_EVICT",
-//						"Cleared cache: " + cacheName
-//				);
-//			}
-//		}
-//	}
+	protected int getCacheTtlInDays() {
+		return cacheTtlInDays;
+	}
 }
