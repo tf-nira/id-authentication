aeca63b46d4a7749243e554bb40ace4cc7462511
Satyam Rajkumar Singh <satyamsingh007007@gmail.com>
2026-05-29 16:44:23 +0530

    Adding OTPCaffeineCacheConfig in authentication-otp-service

---
+++ authentication/authentication-otp-service/src/main/java/io/mosip/authentication/otp/service/config/OTPCaffeineCacheConfig.java
@@ -0,0 +1,35 @@
+package io.mosip.authentication.otp.service.config;
+
+import java.util.concurrent.TimeUnit;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.caffeine.CaffeineCacheManager;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.github.benmanes.caffeine.cache.Caffeine;
+
+@Configuration
+public class OTPCaffeineCacheConfig {
+
+    @Value("${ida-cache-ttl-in-minutes:5}")
+    private long cacheTtlInMinutes;
+
+    @Bean
+    public CacheManager cacheManager() {
+
+        CaffeineCacheManager cacheManager =
+                new CaffeineCacheManager();
+
+        cacheManager.setCaffeine(
+                Caffeine.newBuilder()
+                        .expireAfterWrite(
+                                cacheTtlInMinutes,
+                                TimeUnit.MINUTES)
+                        .maximumSize(10000)
+        );
+
+        return cacheManager;
+    }
+}
