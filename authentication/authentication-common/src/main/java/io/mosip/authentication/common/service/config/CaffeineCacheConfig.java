9d8da32732d77f46ca55d6af1a8b017a302af673
Satyam Rajkumar Singh <satyamsingh007007@gmail.com>
2026-05-29 13:05:15 +0530

    Adding CaffeineCacheConfig

---
+++ authentication/authentication-common/src/main/java/io/mosip/authentication/common/service/config/CaffeineCacheConfig.java
@@ -0,0 +1,35 @@
+package io.mosip.authentication.common.service.config;
+
+import java.util.concurrent.TimeUnit;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.caffeine.CaffeineCacheManager;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.github.benmanes.caffeine.cache.Caffeine;
+
+@Configuration
+public class CaffeineCacheConfig {
+
+    @Value("${ida-cache-ttl-in-minutes}")
+    private long cacheTtlInMinutes;
+
+    @Bean
+    public CacheManager cacheManager() {
+
+        CaffeineCacheManager cacheManager =
+                new CaffeineCacheManager();
+
+        cacheManager.setCaffeine(
+                Caffeine.newBuilder()
+                        .expireAfterWrite(
+                                cacheTtlInMinutes,
+                                TimeUnit.MINUTES)
+                        .maximumSize(10000)
+        );
+
+        return cacheManager;
+    }
+}
