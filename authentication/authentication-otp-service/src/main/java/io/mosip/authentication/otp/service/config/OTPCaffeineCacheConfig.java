51f0d629263d2d44cee537d62da0e55785450c7a
Satyam Rajkumar Singh <satyamsingh007007@gmail.com>
2026-06-01 15:25:31 +0530

    Usage of duration and DurationStyle.

--- authentication/authentication-otp-service/src/main/java/io/mosip/authentication/otp/service/config/OTPCaffeineCacheConfig.java
+++ authentication/authentication-otp-service/src/main/java/io/mosip/authentication/otp/service/config/OTPCaffeineCacheConfig.java
@@ -3,6 +3,7 @@ package io.mosip.authentication.otp.service.config;
 import java.time.Duration;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.convert.DurationStyle;
 import org.springframework.cache.CacheManager;
 import org.springframework.cache.caffeine.CaffeineCacheManager;
 import org.springframework.context.annotation.Bean;
@@ -14,43 +15,15 @@ import com.github.benmanes.caffeine.cache.Caffeine;
 public class OTPCaffeineCacheConfig {
     @Value("${ida-cache-ttl:5m}")
     private String cacheTtl;
-
-
-    private Duration getDuration() {
-
-        String ttl = cacheTtl.trim().toLowerCase();
-
-        if (ttl.endsWith("s")) {
-            return Duration.ofSeconds(
-                    Long.parseLong(ttl.replace("s", ""))
-            );
-        }
-
-        if (ttl.endsWith("m")) {
-            return Duration.ofMinutes(
-                    Long.parseLong(ttl.replace("m", ""))
-            );
-        }
-
-        if (ttl.endsWith("h")) {
-            return Duration.ofHours(
-                    Long.parseLong(ttl.replace("h", ""))
-            );
-        }
-
-        throw new IllegalArgumentException(
-                "Invalid TTL format : " + ttl
-        );
-    }
     @Bean
     public CacheManager cacheManager() {
         CaffeineCacheManager cacheManager = new CaffeineCacheManager();
+        Duration ttl = DurationStyle.detectAndParse(cacheTtl);
         cacheManager.setCaffeine(
                 Caffeine.newBuilder()
-                        .expireAfterWrite(getDuration())
+                        .expireAfterWrite(ttl)
                         .maximumSize(10000)
         );
         return cacheManager;
     }
 }
-
