25f419c6483430fc451a44645a833003a9904ee8
Jagadeesh-T440 <jagadeesh.thiyagarajan@technoforte.co.in>
2026-06-12 10:45:20 +0530

    Update IdAuthenticationErrorConstants.java

--- authentication/authentication-core/src/main/java/io/mosip/authentication/core/constant/IdAuthenticationErrorConstants.java
+++ authentication/authentication-core/src/main/java/io/mosip/authentication/core/constant/IdAuthenticationErrorConstants.java
@@ -67,7 +67,7 @@ public enum IdAuthenticationErrorConstants {
 			"Please capture biometrics within %s seconds of previous biometric capture"),
 	INVALID_BIO_DIGITALID_TIMESTAMP("IDA-MLC-031", "DigitalId of Biometrics not captured within %s seconds of previous biometrics",
 			"Please capture DigitalId of biometrics within %s seconds of previous biometric capture"),
-	DECLARED_DECEASED("IDA-MLC-032", "The person has been declared deceased/deactivated."),
+	DECLARED_DECEASED("IDA-MLC-032", "NIN/ AIN is inactive (Deactivated/Deceased)."),
 	
 	DEMOGRAPHIC_DATA_MISMATCH_LANG("IDA-DEA-001", "Demographic data %s in %s did not match",
 				"Please re-enter your %s in %s"),
